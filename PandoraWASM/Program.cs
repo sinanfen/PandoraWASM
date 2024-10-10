faa95f37bcc59a6daef7cc16f54d2e333fba437c
Sinan Fen <sinanfen@hotmail.com>
2024-10-11 00:49:10 +0300

    WASM - client updated.

--- PandoraWASM/Program.cs
+++ PandoraWASM/Program.cs
@@ -1,9 +1,12 @@
 using Blazored.LocalStorage;
 using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using PandoraWASM;
+using PandoraWASM.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -18,6 +21,20 @@ builder.Services.AddMudServices(config =>
 builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddBlazoredLocalStorage();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("PandoraAPI", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7114/");
+});
+
+// Register HttpClient instances to be used for non-authenticated requests
+builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("PandoraAPI"));
+
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 await builder.Build().RunAsync();
