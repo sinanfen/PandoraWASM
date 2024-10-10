6b1e4d4471716a8bf24b1fd40dd46378a7b89e4b
Sinan Fen <sinanfen@hotmail.com>
2024-10-11 00:49:50 +0300

    add - user service for client

---
+++ PandoraWASM/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Pandora.Shared.DTOs.UserDTOs;
+
+namespace PandoraWASM.Services;
+
+public interface IUserService
+{
+    Task<UserDto> GetUserAsync(Guid userId);
+    Task<List<UserDto>> GetAllUsersAsync();
+    Task<bool> UpdateUserAsync(UserUpdateDto userUpdateDto);
+    Task<bool> DeleteUserAsync(Guid userId);
+}
---
+++ PandoraWASM/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http.Json;
+using Pandora.Shared.DTOs.UserDTOs;
+using PandoraWASM.Services;
+
+public class UserService : IUserService
+{
+    private readonly HttpClient _httpClient;
+
+    public UserService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<UserDto> GetUserAsync(Guid userId)
+    {
+        var response = await _httpClient.GetAsync($"api/users/{userId}");
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadFromJsonAsync<UserDto>();
+        }
+        return null;
+    }
+
+    public async Task<List<UserDto>> GetAllUsersAsync()
+    {
+        var response = await _httpClient.GetAsync("api/users");
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadFromJsonAsync<List<UserDto>>();
+        }
+        return new List<UserDto>();
+    }
+
+    public async Task<bool> UpdateUserAsync(UserUpdateDto userUpdateDto)
+    {
+        var response = await _httpClient.PutAsJsonAsync("api/users/update", userUpdateDto);
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> DeleteUserAsync(Guid userId)
+    {
+        var response = await _httpClient.DeleteAsync($"api/users/delete/{userId}");
+        return response.IsSuccessStatusCode;
+    }
+}
